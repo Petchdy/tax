741e5e745809ff83520d323b453ddba714244745
Petchdy <skulwisit@gmail.com>
2020-04-18 13:30:17 +0700

    t

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,29 +8,31 @@ public class Main {
 	// write your code here
         Scanner in = new Scanner(System.in);
         int x = in.nextInt();
-        if (x>=80){
-            System.out.println("4");
+        if (x>150_000){
+            if (x>=5_000_001){
+                System.out.printf("%.2f",x*0.35);
+            }
+            else if (x>=2_000_001){
+                System.out.printf("%.2f",x*0.30);
+            }
+            else if (x>=1_000_001){
+                System.out.printf("%.2f",x*0.25);
+            }
+            else if (x>=750_001){
+                System.out.printf("%.2f",x*0.20);
+            }
+            else if (x>=500_001){
+                System.out.printf("%.2f",x*0.15);
+            }
+            else if (x>=300_001){
+                System.out.printf("%.2f",x*0.1);
+            }
+            else if (x>=150_000){
+                System.out.printf("%.2f",x*0.05);
+            }
         }
-        else if (x>=75){
-            System.out.println("3.5");
-        }
-        else if (x>=70){
-            System.out.println("3");
-        }
-        else if (x>=65){
-            System.out.println("2.5");
-        }
-        else if (x>=60){
-            System.out.println("2");
-        }
-        else if (x>=55){
-            System.out.println("1.5");
-        }
-        else if (x>=50){
-            System.out.println("1");
-        }
-        else if (x<50){
-            System.out.println("0");
+        else {
+            System.out.println("ไม่ต้องเสียภาษี");
         }
     }
 }
