0750ae1bb7139d899c0a09102a1a1ec761fe9293
Petchdy <skulwisit@gmail.com>
2020-04-18 13:15:12 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        if (x>=80){
+            System.out.println("4");
+        }
+        else if (x>=75){
+            System.out.println("3.5");
+        }
+        else if (x>=70){
+            System.out.println("3");
+        }
+        else if (x>=65){
+            System.out.println("2.5");
+        }
+        else if (x>=60){
+            System.out.println("2");
+        }
+        else if (x>=55){
+            System.out.println("1.5");
+        }
+        else if (x>=50){
+            System.out.println("1");
+        }
+        else if (x<50){
+            System.out.println("0");
+        }
+    }
+}
